b92b6bf8a8d6790d39913c902aaf1b6926f841b6
20641118 <callehj@hotmail.se>
2020-02-07 10:58:07 +0100

    Write tests

---
+++ tests/api.test.js
@@ -0,0 +1,41 @@
+const TwitchApi = require("../index");
+const client = require("../data/apiUser.json");
+
+const api = new TwitchApi({
+	client_id: client.client_id,
+	client_secret: client.client_secret,
+	isApp: true,
+	scopes: ["bits:read", "channel:read:subscriptions"]
+});
+
+
+test("should return array of single username", async () => {
+	const result = await api.getUsers("iamstreaming");
+
+	expect(result.data[0].login).toBe("iamstreaming");
+});
+
+test("should return array", async () => {
+	const result = await api.getFollows({to_id: 48504279});
+
+	expect(result.data.length).toBeGreaterThan(1);
+});
+
+test("should be array with length 0", async () => {
+	const result = await api.getSubsById(48504279);
+
+	expect(result.data).toHaveLength(0);
+});
+
+test("should get array of streams", async () => {
+	const result = await api.getStreams({ first: 1 });
+
+	expect(result.data).toBeInstanceOf(Array);
+	expect(result.data).toHaveLength(1);
+});
+
+test("should return array of videos", async () => {
+	const result = await api.customRequest("/videos?user_id=48504279");
+
+	expect(result.data).toBeInstanceOf(Array);
+});
---
+++ tests/methods.test.js
@@ -0,0 +1,22 @@
+const methods = require("../methods");
+const credentials = require("../data/apiUser.json");
+
+test("should return access token", () => {
+	const result = methods.getLocalAccessToken();
+	expect(result).toBe(credentials.access_token);
+});
+
+test("should return refresh token", () => {
+	const result = methods.getLocalRefreshToken();
+	expect(result).toBe(credentials.refresh_token);
+});
+
+test("should return client id", () => {
+	const result = methods.getLocalClientId();
+	expect(result).toBe(credentials.client_id);
+});
+
+test("should return client secret", () => {
+	const result = methods.getLocalClientSecret();
+	expect(result).toBe(credentials.client_secret);
+});
