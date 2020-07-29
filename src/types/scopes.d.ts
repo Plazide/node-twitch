6ac4f110bf186e263b7b405aa533033a3e04f381
Carl Hallén Jansson <callehj@hotmail.se>
2020-07-29 10:12:10 +0200

    Format scope type and add 'channel:read:stream_key'

--- src/types/scopes.d.ts
+++ src/types/scopes.d.ts
@@ -1,1 +1,15 @@
-export type Scope = "analytics:read:extensions" | "analytics:read:games" | "bits:read" | "channel:edit:commercial" | "channel:read:hype_train" | "channel:read:subscriptions" | "clips:edit" | "user:edit" | "user:edit:broadcast" | "user:edit:follows" | "user:read:broadcast" | "user:read:email" | "user:read:stream_key" | "moderation:read";
+export type Scope =
+	"analytics:read:extensions" |
+	"analytics:read:games" |
+	"bits:read" |
+	"channel:edit:commercial" |
+	"channel:read:hype_train" |
+	"channel:read:subscriptions" |
+	"channel:read:stream_key" |
+	"clips:edit" |
+	"user:edit" |
+	"user:edit:broadcast" |
+	"user:edit:follows" |
+	"user:read:broadcast" |
+	"user:read:email" |
+	"moderation:read";
